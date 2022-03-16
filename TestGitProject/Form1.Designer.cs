b7d469aa6e20b971ebe14bb556121f3472e4c81d
Цветков Киллриал <kiryatsvetkov@mail.ru>
2022-03-16 23:04:06 +0300

    Чайник с функцией жопа

--- TestGitProject/Form1.Designer.cs
+++ TestGitProject/Form1.Designer.cs
@@ -33,22 +33,25 @@ namespace TestGitProject
             this.button1 = new System.Windows.Forms.Button();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(350, 82);
+            this.label1.Location = new System.Drawing.Point(467, 101);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.Size = new System.Drawing.Size(117, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "ВСЕМ ЗДАРОВА";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(99, 163);
+            this.button1.Location = new System.Drawing.Point(132, 201);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(545, 23);
+            this.button1.Size = new System.Drawing.Size(727, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "Сделать хорошо";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,9 +59,10 @@ namespace TestGitProject
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(220, 256);
+            this.radioButton1.Location = new System.Drawing.Point(293, 315);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(56, 17);
+            this.radioButton1.Size = new System.Drawing.Size(67, 21);
             this.radioButton1.TabIndex = 2;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "жесть";
@@ -66,22 +70,34 @@ namespace TestGitProject
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(616, 376);
+            this.button2.Location = new System.Drawing.Point(821, 463);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 3;
             this.button2.Text = "Ок";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(664, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(257, 152);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -95,6 +111,7 @@ namespace TestGitProject
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
